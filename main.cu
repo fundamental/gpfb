531d51ca0e96ffd5614eff34c93431ccd163341d
Mark McCurry <mmccurry@haystack.mit.edu>
2011-06-15 13:24:11 -0400

    Updating FIR impl to matlab spec

--- main.cu
+++ main.cu
@@ -19,12 +19,27 @@ float sinc(float x)
         return sin(x)/x;
 }
 
+/* Put fir coeffs into buf with length taps
+ * The filter has a corner frequency at fc (normalized)
+ */
 float *gen_fir(float *buf, unsigned taps, float fc)
 {
     assert(!(taps%2));
 
     for(size_t i=0; i<taps; ++i)
         buf[i] = sinc(PI*fc*(i-taps/2.0))*fc;
+    return buf;
+}
+
+//Rescale FIR filter coeff
+float *scale_fir(float *buf, unsigned N)
+{
+    float max = 0.0;
+    for(size_t i=0; i<N; ++i)
+        max = buf[i]>max?buf[i]:max;
+
+    for(size_t i=0; i<N; ++i)
+        buf[i] /= max;
     return buf;
 }
 
@@ -85,14 +100,15 @@ float *gen_chirp(float *buf, size_t N, size_t period, double dr)
 __global__ void convolve(float *coeff, size_t N, float *src, size_t M, float *dest, size_t chans)
 {
     int i = blockIdx.x * blockDim.x + threadIdx.x;
-    if (i>M) 
+    if (i>M)
         return;
+
+    unsigned sel = i%chans;
 
     //do actual work at i
     dest[i] = 0.0;
-    for(size_t j=0; j<N; ++j)
-        if(j%chans==0)
-            dest[i] += src[i-j]*coeff[j];
+    for(size_t j=sel; j<N; j+=chans)
+        dest[i] += src[i-sel-j]*coeff[j];
 }
 
 #undef checked
@@ -122,6 +138,9 @@ float *apply_fir(float *buf, size_t N, float *coeff, size_t M, size_t chans)
     int blocks = N/block_size + (N%block_size == 0 ? 0:1);
     convolve <<< blocks, block_size >>>(cu_coeff, M, cu_buf+M, N, cu_smps, chans);
 
+    //Post Process
+    //apply_fft(cu_smps, chans, N/chans);
+
     //Retreive
     puts("Getting...");
     checked(cudaMemcpy(buf, cu_smps, sizeof(float)*N, cudaMemcpyDeviceToHost));
@@ -137,17 +156,23 @@ float *apply_fir(float *buf, size_t N, float *coeff, size_t M, size_t chans)
 
 int main()
 {
-    const size_t CHANNELS = 4,
+    const size_t CHANNELS = 8,
                  N        = CHANNELS*8;
     float fir[N];
-    gen_fir(fir, N, 1.0/CHANNELS);
+    gen_fir(fir, N, 0.5/CHANNELS);
+    scale_fir(fir, N);
 
-    const size_t M=N*128;
+    const size_t M=N*FRAMES;
     float buf[M+N];
-    memset(buf, 0, N*sizeof(float));
+    memset(buf, 0, (M+N)*sizeof(float));
     float *smps=buf+N;
     //gen_rand(smps, M, 1.0);
-    gen_saw(smps, M, 1024);
+    //gen_saw(smps, M, 278);
+    gen_chirp(smps, M, 1024*16, 0.0008);
+    //gen_sin(smps, M, 8);
+    //gen_sin(smps, M, 16);
+    gen_sin(smps, FS, 500.0);
+    //gen_imp(smps,M);
 
     //Show previous
     FILE *fb = fopen("before.txt", "w+");
@@ -161,7 +186,7 @@ int main()
     //Show results
     FILE *fa = fopen("after.txt", "w+");
     for(size_t i=0;i<M;++i)
-        fprintf(fa, "%f, ", smps[i]);
+        fprintf(fa, "%c%f", i?',':' ', smps[i]);
     fclose(fa);
     return 0;
 }
